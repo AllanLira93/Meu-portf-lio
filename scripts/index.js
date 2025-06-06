7f18f75fb2598464e79638297404869a27f5137d
AllanLira93 <allanlirar12@gmail.com>
2025-06-06 18:22:10 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,27 @@
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
 
-document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Impede o envio do formulário
-    window.alert("Formulário enviado com sucesso!"); 
-    this.reset(); // reseta o formulário
+    // Coleta os dados do formulário
+    const formData = new FormData(contactForm);
+
+    // Enviar os dados para o serviço de e-mail 
+    fetch(contactForm.action, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            // Exibir uma mensagem de sucesso ao usuário
+            alert('Mensagem enviada com sucesso!');
+            // Limpar o formulário
+            contactForm.reset();
+        } else {
+            // Exibir uma mensagem de erro ao usuário
+            alert('Ocorreu um erro ao enviar a mensagem. Por favor tente novamente.');
+        }
+    })
+    .catch(error => {
+        console.error('Erro:', error); // Logar o erro no console para depuração
+        alert('Ocorreu um erro ao enviar a mensagem. Por favor tente novamente.');
+    });
 });
